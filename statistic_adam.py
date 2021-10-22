1256ae9bc0377a267ffd9635724c339e80e256dd
Adam Bin Azmi <92119571+adamazm@users.noreply.github.com>
2021-10-22 12:47:01 +0200

    Add files via upload

--- statistic_adam.py
+++ statistic_adam.py
@@ -3,22 +3,27 @@ import json
 #count ip adress
 
 def statIP(nomFicJSON) :
-    ipAddr=[]
-    cntr=[]
-    i=0
-    j=0 
+    statIP={}
     with open(nomFicJSON,"r") as f :
         dict1=json.load(f)
     for data in dict1:
-        if data['remote_ip'] not in ipAddr:
-            ipAddr.append(data['remote_ip'])
-            cntr[i]=j+1
-            i=i+1
-        else:
+        if data['remote_ip'] not in statIP:
+            statIP[data['remote_ip']]=1
             
+        else:
+            statIP[data['remote_ip']]=statIP[data['remote_ip']]+1
     
     return statIP
 
 #count browser
 def statBrowser(line) :
+    statBrowser={}
+    with open(nomFicJSON,"r") as f :
+        dict1=json.load(f)
+    for data in dict1:
+        if data['browser'] not in statBrowser:
+            statIP[data['browser']]=1
+            
+        else:
+            statIP[data['browser']]=statIP[data['browser']]+1   
     return statBrowser
